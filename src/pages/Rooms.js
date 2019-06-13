cf7a184c1d6c7f3737058f95e3b552be3feb63bd
Anuragkrishh <anuragkrishna95@gmail.com>
2019-06-13 23:32:54 +0530

    Banner, Error & Services

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import {Link} from 'react-router-dom'
+import Banner from '../components/Banner'
 export default function Rooms() {
     return (
-        <Hero hero="roomsHero"/>
+        <Hero hero="roomsHero">
+            <Banner title="rooms">
+                <Link to='/'
+                className="btn-primary">
+                    return home
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
